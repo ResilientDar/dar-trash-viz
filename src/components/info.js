52ce5e6d52e70612e4790cd9cfa418963b985319
Samweli Mwakisambwe <smwltwesa6@gmail.com>
2018-11-22 12:43:56 +0300

    Update info.js

--- src/components/info.js
+++ src/components/info.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import TextEllipsis from 'react-text-ellipsis'
-import { Modal, Button } from 'react-bootstrap'
 
 
 let Info = class Info extends React.Component {
